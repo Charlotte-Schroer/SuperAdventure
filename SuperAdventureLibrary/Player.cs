01c4e21b274645740491b432f8fb128fd4c72ec0
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-10-27 11:02:55 +0100

    Creating Player class and its proporties

--- SuperAdventureLibrary/Player.cs
+++ SuperAdventureLibrary/Player.cs
@@ -9,7 +9,7 @@ namespace SuperAdventureLibrary
     public class Player
     {
         public int CurrentHitPoints { get; set; }
-        public int MaximumPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
         public int Gold {  get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
